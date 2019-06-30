0f3567812e5326a4c41f5c0de38094db546b8fc9
Artur Iablokov <artyapple@gmail.com>
2019-06-30 14:19:45 +0200

    http request

--- src/services/etherium-service.js
+++ src/services/etherium-service.js
@@ -1,180 +1,209 @@
 const Web3 = require('web3');
 const Accounts = require('web3-eth-accounts');
+const request = require('request');
 
+function callback(error, response, body) {
+  console.log('response: ', response.statusCode)
+  if (!error && response.statusCode == 200) {
+    console.log('response: ', response);
+    console.log('not error:!! ');
+  } else {
+    console.log('response: ', response);
+    console.log('error!!!!');
+  }
+}
 
 module.exports = class EtheriumService {
 
-    transact() {
-        return new Promise((resolve, reject) => {
-            let web3;
-            if (typeof web3 !== 'undefined') {
-                web3 = new Web3(web3.currentProvider);
-            } else {
-                // set the provider you want from Web3.providers
-                web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8001"));
-            }
 
-            //const accounts = new Accounts('http://localhost:8001', null, options);
 
-            // web3.eth.defaultAccount = web3.eth.accounts[0];
-            web3.eth.defaultAccount = '0x62a86f3cae24e6bdcf10bd616cfdb7049c04f745';
-            console.log('web3.eth.defaultAccount', web3.eth.defaultAccount);
+  transact() {
+    return new Promise((resolve, reject) => {
+      let web3;
+      if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+      } else {
+        // set the provider you want from Web3.providers
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8001"));
+      }
 
-            web3.eth.personal.unlockAccount(web3.eth.defaultAccount, "haw", 15000).then((data) => {console.log(data)
-              var CoursetroContract = web3.eth.Contract([{
-                  constant: true,
-                  inputs: [],
-                  name: "get_device_count",
-                  outputs: [{
-                      name: "count",
-                      type: "uint256"
-                  }],
-                  payable: false,
-                  stateMutability: "view",
-                  type: "function"
-              }, {
-                  constant: false,
-                  inputs: [],
-                  name: "kill",
-                  outputs: [],
-                  payable: false,
-                  stateMutability: "nonpayable",
-                  type: "function"
-              }, {
-                  constant: true,
-                  inputs: [{
-                      name: "device_id",
-                      type: "address"
-                  }],
-                  name: "get_device_timestamps",
-                  outputs: [{
-                      name: "timestamp",
-                      type: "uint256[]"
-                  }],
-                  payable: false,
-                  stateMutability: "view",
-                  type: "function"
-              }, {
-                  constant: false,
-                  inputs: [{
-                      name: "device_id",
-                      type: "address"
-                  }, {
-                      name: "filehash",
-                      type: "string"
-                  }],
-                  name: "set_device_data",
-                  outputs: [{
-                      name: "index",
-                      type: "uint256"
-                  }, {
-                      name: "timestamp",
-                      type: "uint256"
-                  }],
-                  payable: false,
-                  stateMutability: "nonpayable",
-                  type: "function"
-              }, {
-                  constant: true,
-                  inputs: [{
-                      name: "device_id",
-                      type: "address"
-                  }],
-                  name: "is_device_present",
-                  outputs: [{
-                      name: "result",
-                      type: "bool"
-                  }],
-                  payable: false,
-                  stateMutability: "view",
-                  type: "function"
-              }, {
-                  constant: true,
-                  inputs: [{
-                      name: "device_id",
-                      type: "address"
-                  }, {
-                      name: "timestamp",
-                      type: "uint256"
-                  }],
-                  name: "get_device_data",
-                  outputs: [{
-                      name: "hash",
-                      type: "string"
-                  }],
-                  payable: false,
-                  stateMutability: "view",
-                  type: "function"
-              }, {
-                  constant: true,
-                  inputs: [{
-                      name: "index",
-                      type: "uint256"
-                  }],
-                  name: "get_device_at_index",
-                  outputs: [{
-                      name: "device_address",
-                      type: "address"
-                  }],
-                  payable: false,
-                  stateMutability: "view",
-                  type: "function"
-              }, {
-                  inputs: [],
-                  payable: false,
-                  stateMutability: "nonpayable",
-                  type: "constructor"
-              }, {
-                  anonymous: false,
-                  inputs: [{
-                      indexed: true,
-                      name: "device_id",
-                      type: "address"
-                  }, {
-                      indexed: false,
-                      name: "index",
-                      type: "uint256"
-                  }, {
-                      indexed: false,
-                      name: "timestamp",
-                      type: "uint256"
-                  }, {
-                      indexed: false,
-                      name: "filehash",
-                      type: "string"
-                  }],
-                  name: "log_action",
-                  type: "event"
-              }], '0x6bb79673638386196357b1b9032219aba6bf3c10'
-              );
+      //const accounts = new Accounts('http://localhost:8001', null, options);
 
-              CoursetroContract.methods.set_device_data(web3.eth.defaultAccount, 'other-some-test-hash').send({ from: web3.eth.defaultAccount })
-                  .then((data) => {
-                      console.log('get_device_timestamps', `Status ${data.status}`, `Transaction Hash ${data.transactionHash}`);
-                      CoursetroContract.methods.get_device_timestamps(web3.eth.defaultAccount).call()
-                          .then((data) => {
-                              console.log('get_device_timestamps', `Length ${data.length}`, `Last BigNumber ${data[data.length -1]}`);
-                              CoursetroContract.methods.get_device_data(web3.eth.defaultAccount, data[data.length -1]).call().then((value) =>{
-                                console.log('Value from blockchain: ', value);
-                                resolve(value);
-                              }).catch((err) => {
-                                console.error(err)
-                                reject(err);
-                              });
+      // web3.eth.defaultAccount = web3.eth.accounts[0];
+      web3.eth.defaultAccount = '0x62a86f3cae24e6bdcf10bd616cfdb7049c04f745';
+      console.log('web3.eth.defaultAccount', web3.eth.defaultAccount);
 
-                          }).catch((err) => {
-                              console.error(err)
-                              reject(err);
-                          });
-                  }).catch((err) => {
-                      console.error(err)
-                      reject(err);
-                  });
+      web3.eth.personal.unlockAccount(web3.eth.defaultAccount, "haw", 15000).then((data) => {
+        console.log(data)
+        var CoursetroContract = web3.eth.Contract([{
+          constant: true,
+          inputs: [],
+          name: "get_device_count",
+          outputs: [{
+            name: "count",
+            type: "uint256"
+          }],
+          payable: false,
+          stateMutability: "view",
+          type: "function"
+        }, {
+          constant: false,
+          inputs: [],
+          name: "kill",
+          outputs: [],
+          payable: false,
+          stateMutability: "nonpayable",
+          type: "function"
+        }, {
+          constant: true,
+          inputs: [{
+            name: "device_id",
+            type: "address"
+          }],
+          name: "get_device_timestamps",
+          outputs: [{
+            name: "timestamp",
+            type: "uint256[]"
+          }],
+          payable: false,
+          stateMutability: "view",
+          type: "function"
+        }, {
+          constant: false,
+          inputs: [{
+            name: "device_id",
+            type: "address"
+          }, {
+            name: "filehash",
+            type: "string"
+          }],
+          name: "set_device_data",
+          outputs: [{
+            name: "index",
+            type: "uint256"
+          }, {
+            name: "timestamp",
+            type: "uint256"
+          }],
+          payable: false,
+          stateMutability: "nonpayable",
+          type: "function"
+        }, {
+          constant: true,
+          inputs: [{
+            name: "device_id",
+            type: "address"
+          }],
+          name: "is_device_present",
+          outputs: [{
+            name: "result",
+            type: "bool"
+          }],
+          payable: false,
+          stateMutability: "view",
+          type: "function"
+        }, {
+          constant: true,
+          inputs: [{
+            name: "device_id",
+            type: "address"
+          }, {
+            name: "timestamp",
+            type: "uint256"
+          }],
+          name: "get_device_data",
+          outputs: [{
+            name: "hash",
+            type: "string"
+          }],
+          payable: false,
+          stateMutability: "view",
+          type: "function"
+        }, {
+          constant: true,
+          inputs: [{
+            name: "index",
+            type: "uint256"
+          }],
+          name: "get_device_at_index",
+          outputs: [{
+            name: "device_address",
+            type: "address"
+          }],
+          payable: false,
+          stateMutability: "view",
+          type: "function"
+        }, {
+          inputs: [],
+          payable: false,
+          stateMutability: "nonpayable",
+          type: "constructor"
+        }, {
+          anonymous: false,
+          inputs: [{
+            indexed: true,
+            name: "device_id",
+            type: "address"
+          }, {
+            indexed: false,
+            name: "index",
+            type: "uint256"
+          }, {
+            indexed: false,
+            name: "timestamp",
+            type: "uint256"
+          }, {
+            indexed: false,
+            name: "filehash",
+            type: "string"
+          }],
+          name: "log_action",
+          type: "event"
+        }], '0x6bb79673638386196357b1b9032219aba6bf3c10');
 
-            }).catch((err) => {
-              console.log(err)
-              reject(err);
-            });
-        });
-    }
+        const options = {
+          url: 'http://localhost:8500/bzz:/',
+          headers: {
+            'Content-Type': 'text/plain'
+          },
+          method: 'POST',
+          body: '{ "timestamp": "1505477559", "payload": "hello from IoT device! ai" }'
+        };
+
+        request(options, callback).then((response) => {
+          console.log("then request")
+        })
+
+
+        CoursetroContract.methods.set_device_data(web3.eth.defaultAccount, 'other-some-test-hash').send({
+            from: web3.eth.defaultAccount
+          })
+          .then((data) => {
+            console.log('get_device_timestamps', `Status ${data.status}`, `Transaction Hash ${data.transactionHash}`);
+            CoursetroContract.methods.get_device_timestamps(web3.eth.defaultAccount).call()
+              .then((data) => {
+                console.log('get_device_timestamps', `Length ${data.length}`, `Last BigNumber ${data[data.length -1]}`);
+                CoursetroContract.methods.get_device_data(web3.eth.defaultAccount, data[data.length - 1]).call().then((value) => {
+                  console.log('Value from blockchain: ', value);
+                  resolve(value);
+                }).catch((err) => {
+                  console.error(err)
+                  reject(err);
+                });
+
+              }).catch((err) => {
+                console.error(err)
+                reject(err);
+              });
+          }).catch((err) => {
+            console.error(err)
+            reject(err);
+          });
+
+      }).catch((err) => {
+        console.log(err)
+        reject(err);
+      });
+    });
+  }
 }
