796fd6f38d0257958bf49342746cd3e8c2fe5361
Artur Iablokov <artyapple@gmail.com>
2019-07-06 06:37:52 +0200

    stable test

--- src/app.js
+++ src/app.js
@@ -9,12 +9,42 @@ let main = () => {
   controller.register('#tr_2', 4, 600000);
   // after 20 min
   controller.register('#tr_3', 5, 1200000);
+
+  controller.register('#tr_1', 6, 0);
+  // after 10 min
+  controller.register('#tr_2', 7, 600000);
+  // after 20 min
+  controller.register('#tr_3', 8, 1200000);
+
+  controller.register('#tr_1', 3, 0);
+  // after 10 min
+  controller.register('#tr_2', 4, 600000);
+  // after 20 min
+  controller.register('#tr_3', 5, 1200000);
+
+  controller.register('#tr_1', 6, 0);
+  // after 10 min
+  controller.register('#tr_2', 7, 600000);
+  // after 20 min
+  controller.register('#tr_3', 8, 1200000);
+
+  controller.register('#tr_1', 3, 0);
+  // after 10 min
+  controller.register('#tr_2', 4, 600000);
+  // after 20 min
+  controller.register('#tr_3', 5, 1200000);
+
+  controller.register('#tr_1', 10, 0);
+  // after 10 min
+  controller.register('#tr_2', 10, 600000);
+  // after 20 min
+  controller.register('#tr_3', 10, 1200000);
   //controller.register('#tr_3', 15, 50000);
   // controller.otherRun();
 
 
   console.log('start: ', new Date().toISOString());
-  var j = schedule.scheduleJob('*/10 * * * *', function() {
+  var j = schedule.scheduleJob('*/30 * * * *', function() {
     if (controller.collection.completed) {
       console.log('finish', new Date().toISOString());
       return;
