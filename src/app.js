6337cffe8e840e0248d749d14ca401aef1b5e251
Your Name <you@example.com>
2019-07-06 18:19:46 +0000

    some fixes

--- src/app.js
+++ src/app.js
@@ -10,41 +10,8 @@ let main = () => {
   // after 20 min
   controller.register('#tr_3', 5, 1200000);
 
-  controller.register('#tr_1', 6, 0);
-  // after 10 min
-  controller.register('#tr_2', 7, 600000);
-  // after 20 min
-  controller.register('#tr_3', 8, 1200000);
-
-  controller.register('#tr_1', 3, 0);
-  // after 10 min
-  controller.register('#tr_2', 4, 600000);
-  // after 20 min
-  controller.register('#tr_3', 5, 1200000);
-
-  controller.register('#tr_1', 6, 0);
-  // after 10 min
-  controller.register('#tr_2', 7, 600000);
-  // after 20 min
-  controller.register('#tr_3', 8, 1200000);
-
-  controller.register('#tr_1', 3, 0);
-  // after 10 min
-  controller.register('#tr_2', 4, 600000);
-  // after 20 min
-  controller.register('#tr_3', 5, 1200000);
-
-  controller.register('#tr_1', 10, 0);
-  // after 10 min
-  controller.register('#tr_2', 10, 600000);
-  // after 20 min
-  controller.register('#tr_3', 10, 1200000);
-  //controller.register('#tr_3', 15, 50000);
-  // controller.otherRun();
-
-
   console.log('start: ', new Date().toISOString());
-  var j = schedule.scheduleJob('*/30 * * * *', function() {
+  var j = schedule.scheduleJob('*/2 * * * *', function() {
     if (controller.collection.completed) {
       console.log('finish', new Date().toISOString());
       return;
