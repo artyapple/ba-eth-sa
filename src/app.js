649708664884076de14a43aa9fee3a739a58bde4
Artur Iablokov <artyapple@gmail.com>
2019-07-07 04:35:07 +0200

    run n1

--- src/app.js
+++ src/app.js
@@ -4,24 +4,44 @@ const getSize = require('get-folder-size');
 
 let main = () => {
   const controller = new TransactionController();
-  controller.register('#tr_1', 3);
+
+  controller.register('#tr_1_1', 5);
+  controller.register('#tr_1_2', 5);
+  controller.register('#tr_1_3', 5);
   // after 10 min
-  controller.register('#tr_2', 4);
-  // after 20 min
-  controller.register('#tr_3', 5);
+  controller.register('#tr_2_1', 10);
+  controller.register('#tr_2_2', 10);
+  controller.register('#tr_2_3', 10);
 
-  controller.register('#tr_4', 6);
+  controller.register('#tr_3_1', 15);
+  controller.register('#tr_3_2', 15);
+  controller.register('#tr_3_3', 15);
 
-  controller.register('#tr_5', 7);
+  controller.register('#tr_4_1', 20);
+  controller.register('#tr_4_2', 20);
+  controller.register('#tr_4_3', 20);
 
-  controller.register('#tr_6', 8);
+  controller.register('#tr_5_1', 25);
+  controller.register('#tr_5_2', 25);
+  controller.register('#tr_5_3', 25);
 
+  controller.register('#tr_6_1', 30);
+  controller.register('#tr_6_2', 30);
+  controller.register('#tr_6_3', 30);
+
+  controller.register('#tr_7_1', 35);
+  controller.register('#tr_7_2', 35);
+  controller.register('#tr_7_3', 35);
+
+  controller.register('#tr_8_1', 40);
+  controller.register('#tr_8_2', 40);
+  controller.register('#tr_8_3', 40);
   //controller.register('#tr_3', 15, 50000);
   // controller.otherRun();
 
 
   console.log('start: ', new Date().toISOString());
-  var j = schedule.scheduleJob('*/10 * * * *', function() {
+  var j = schedule.scheduleJob('0 * * * *', function() {
     if (controller.collection.completed) {
       console.log('finish', new Date().toISOString());
       return;
